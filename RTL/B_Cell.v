f9d1a0b28397a86d81bda6f6c712078d7d86e06a
Ho97 <120174812+Ho97@users.noreply.github.com>
2022-12-24 00:33:35 +0900

    Delete B_Cell.v

--- RTL/B_Cell.v
+++
@@ -1,9 +0,0 @@
-
-module B_Cell(G0, G1, P0, P1, PP, GG);
-	input G0, G1, P0, P1;
-	output GG, PP;
-	
-	assign GG = G1 | (P1 & G0);
-	assign PP = P0 & P1;
-endmodule
-//B_Cell = G_k-1:l, G_i:k, P_k-1:l, P_i:k >> G_i:l, P_i:i
